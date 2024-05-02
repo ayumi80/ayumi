0c64868c585922e3f1abd90eca8cb1b04d443196
ayumi80 <164593931+ayumi80@users.noreply.github.com>
2024-05-02 23:42:19 +0800

    Add files via upload

---
+++ data.php
@@ -0,0 +1,21 @@
+<?php
+
+$data = [
+    [
+        'intansi' => 'SD No 1 Tanjung Benoa',
+        'tahun' => '2010 - 2015',
+        'status' => 'Lulus',
+    
+    ],
+    [
+        'intansi' => 'SMP Negeri 3 Kuta Selatan',
+        'tahun' => '2015 - 2018',
+        'status' => 'Lulus',
+    ],
+    [
+        'intansi' => 'SMK Negeri 1 Kuta Selatan',
+        'tahun' => '2018 - 2021',
+        'status' => 'Lulus',
+    
+    ],
+];
